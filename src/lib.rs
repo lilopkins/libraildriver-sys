773ce6519f9097305885684283f91c355314281b
Lily Hopkins <bsalarius@gmail.com>
2019-09-27 22:20:27 +0100

    0.1.2: Changed to dynamic library loading.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,54 @@
 #![allow(non_snake_case)]
-
 extern crate libc;
+extern crate libloading as lib;
 
-#[link(name = "RailDriver")]
-extern "C" {
-    pub fn GetRailSimValue(id: libc::c_int, modifier: libc::c_int) -> libc::c_float;
-    pub fn SetRailSimValue(id: libc::c_int, value: libc::c_int);
+use libc::{c_int, c_float};
 
-    pub fn SetRailDriverConnected(isConnected: bool);
-    pub fn GetNextRailDriverId(start: libc::c_int) -> libc::c_int;
-    pub fn GetRailDriverValue(id: libc::c_int) -> libc::c_float;
+#[cfg(target_arch = "x86")]
+unsafe fn get_library() -> lib::Library {
+    lib::Library::new("RailDriver.dll").unwrap()
+}
 
-    pub fn GetRailSimConnected() -> bool;
-    pub fn SetRailSimConnected(isConnected: bool);
-    pub fn GetRailSimLocoChanged() -> bool;
-    pub fn GetRailSimCombinedThrottleBrake() -> bool;
+#[cfg(target_arch = "x86_64")]
+unsafe fn get_library() -> lib::Library {
+    lib::Library::new("RailDriver64.dll").unwrap()
+}
+
+pub unsafe fn GetRailSimValue(id: c_int, modifier: c_int) -> c_float {
+    let rd = get_library();
+    rd.get::<unsafe extern fn(c_int, c_int) -> c_float>(b"GetRailSimValue").unwrap()(id, modifier)
+}
+pub unsafe fn SetRailSimValue(id: c_int, value: c_int) {
+    let rd = get_library();
+    rd.get::<unsafe extern fn(c_int, c_int)>(b"SetRailSimValue").unwrap()(id, value)
+}
+
+pub unsafe fn SetRailDriverConnected(isConnected: bool) {
+    let rd = get_library();
+    rd.get::<unsafe extern fn(bool)>(b"SetRailDriverConnected").unwrap()(isConnected)
+}
+pub unsafe fn GetNextRailDriverId(start: c_int) -> c_int {
+    let rd = get_library();
+    rd.get::<unsafe extern fn(c_int) -> c_int>(b"GetNextRailDriverId").unwrap()(start)
+}
+pub unsafe fn GetRailDriverValue(id: c_int) -> c_float {
+    let rd = get_library();
+    rd.get::<unsafe extern fn(c_int) -> c_float>(b"GetRailDriverValue").unwrap()(id)
+}
+
+pub unsafe fn GetRailSimConnected() -> bool {
+    let rd = get_library();
+    rd.get::<unsafe extern fn() -> bool>(b"GetRailSimConnected").unwrap()()
+}
+pub unsafe fn SetRailSimConnected(isConnected: bool) {
+    let rd = get_library();
+    rd.get::<unsafe extern fn(bool)>(b"SetRailSimConnected").unwrap()(isConnected)
+}
+pub unsafe fn GetRailSimLocoChanged() -> bool {
+    let rd = get_library();
+    rd.get::<unsafe extern fn() -> bool>(b"GetRailSimLocoChanged").unwrap()()
+}
+pub unsafe fn GetRailSimCombinedThrottleBrake() -> bool {
+    let rd = get_library();
+    rd.get::<unsafe extern fn() -> bool>(b"GetRailSimCombinedThrottleBrake").unwrap()()
 }
